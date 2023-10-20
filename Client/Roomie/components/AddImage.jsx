075fc25036571170b3cf9fc112306c21830d7b32
S00213963 <stephenmeehan1996@gmail.com>
2023-10-20 09:32:38 +0100

    Multiple image upload working

--- Client/Roomie/components/AddImage.jsx
+++ Client/Roomie/components/AddImage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet,Platform,TextInput,Dimensions,TouchableOpacity,Image,ScrollView, SafeAreaView, Alert} from 'react-native';
+import { View, Text, StyleSheet,Platform,TextInput,Dimensions,TouchableOpacity,Image,ScrollView, SafeAreaView, Alert, FlatList, ActivityIndicator} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 import { FIREBASE_APP, FIREBASE_STORAGE } from '../FirebaseConfig';
@@ -8,11 +8,12 @@ import { Picker } from '@react-native-picker/picker';
 import { DatePickerModal } from 'react-native-paper-dates';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { MultiSelect, Dropdown } from 'react-native-element-dropdown';
+import { launchCameraAsync } from 'expo-image-picker';
 //import { firebase } from '../config'
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
-
+//PermissionsAndroid,
 import { genderOptions, workingHoursOptions, occupationOptions,yearOfStudyOptions,yesNO, rentalPreference, environmentOptions, houseMatExpectations ,irishCounties, number, houseType, priceRange, days, roomType } from '../data/formData';
 import  styles  from '../styles/formStyle.style';
 
@@ -20,11 +21,12 @@ const AddImage = ({navigation}) => {
 
   const [image, setImage] = useState(null);
   const [uploading, setUploading] = useState(false);
-  const [URI, setURI] = useState(false);
-  let uri = '';
-  let filename = '';
+
+  const [selectedFiles, setSelectedFiles] = useState([]);
+ 
 
   const pickImage = async () =>{
+    
     let result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.All,
         allowsEditing: true,
@@ -33,45 +35,67 @@ const AddImage = ({navigation}) => {
         base64: false,
     })
     if (!result.canceled) {
-      setImage(result.assets[0].uri);
+      let uri = result.assets[0].uri;
+      let filename = uri.substring(uri.lastIndexOf('/') + 1);
+      const newImage = {uri: uri, filename: filename };
+      setSelectedFiles([...selectedFiles, newImage]);
     }
   }
 
   const takeImage = async () =>{
-    let result = await ImagePicker.launchCameraAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4,3],
-        quality: 1,
-        base64: false,
-    })
+    let result = await launchCameraAsync()
     if (!result.canceled) {
       setImage(result.assets[0].uri);
     }
   }
 
+  async function pickImageFromCamera() {
+    // const granted = await PermissionsAndroid.request(
+    //   PermissionsAndroid.PERMISSIONS.CAMERA
+    // )
+  
+    // if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    //  // setOpenCamera(true)
+  
+      const result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 3],
+        base64: true,
+        quality: 1
+      })
+  
+      if (!result.canceled) {
+        setImage(result.assets[0].uri);
+      }
+    //}
+  }
+
   const uploadFile = async () =>{
-      try{
-        if (image) {
-        const response = await fetch(image);
-        const blob = await response.blob();
-
-        const filename = image.substring(image.lastIndexOf('/') + 1);
-
-        const storage = FIREBASE_STORAGE;
-        const storageRef = ref(storage, 'images/' + filename);
-        
-        uploadBytes(storageRef, blob).then((snapshot) => {
-          console.log('Uploaded a blob or file!');
-          getDownloadURL(snapshot.ref).then( url => console.log(url));
-        });
-      } else{
+    
+    if (selectedFiles) {
+      const storage = getStorage();
+      const urls = [];
+      setUploading(true);
+      for(const file of selectedFiles){
+       
+        try{
+          const response = await fetch(file.uri);
+          const blob = await response.blob();
+          const storageRef = ref(storage, 'images/' + file.filename);
+          
+          uploadBytes(storageRef, blob).then((snapshot) => {
+            console.log('Uploaded a blob or file!');
+            getDownloadURL(snapshot.ref).then( url => console.log(url));
+            if(file === selectedFiles[selectedFiles.length-1])
+              setUploading(false);
+          });
+        } catch (error) {
+          console.error('Error uploading image:', error);
+        }
+      }
+    } else{
         console.log('No image selected for upload.');
-      }
-
-      } catch (error) {
-        console.error('Error uploading image:', error);
-      }
+    }
   }
 
   return (
@@ -94,25 +118,46 @@ const AddImage = ({navigation}) => {
 
             <Card elevation={5} style={styles.card}>
               <Card.Content>
-                  <TouchableOpacity onPress={pickImage} style={styles2.selectButton}>
-                    <Text style={styles2.btnText}>Pick an Image</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity onPress={takeImage} style={styles2.selectButton}>
-                    <Text style={styles2.btnText}>Take Photo</Text>
-                  </TouchableOpacity>
-                  <View>
-                    {image &&
-                        <Image
-                          source={{uri : image}}
-                          style = {{width: 300, height: 300}}
-                        >
-                        </Image>
-                    }
-                        <TouchableOpacity onPress={uploadFile} style={styles2.selectButton}>
-                           <Text style={styles2.btnText}>upload Image</Text>
-                      </TouchableOpacity>
-
+                <View>
+                 {uploading? <View ><ActivityIndicator size="large" color="#0000ff"/></View>
+                  : <>
+                  </>}
+                  <FlatList
+                      data={selectedFiles}
+                      keyExtractor={(item, index) => index.toString()}
+                      horizontal
+                      renderItem={({ item }) => (
+                        <Image source={item} style={{ width: 100, height: 100, margin: 5 }} />
+                  )}
+                  />
+                </View>
+                <View>
+                  <View style={{ flexDirection: 'row', marginVertical: 10, justifyContent: 'center'}}>
+                    <Button
+                      mode="contained"
+                      style={{marginRight: 10}} // Use "outlined" for an outlined button
+                      color="#FF5733" // Set your desired button color
+                      labelStyle={styles.buttonLabel} // Apply custom label text style // Apply custom button style
+                      onPress={pickImage}>
+                      Pick File
+                  </Button>
+                  <Button
+                      mode="contained" // Use "outlined" for an outlined button
+                      color="#FF5733" // Set your desired button color
+                      labelStyle={styles.buttonLabel} // Apply custom label text style // Apply custom button style
+                      onPress={pickImageFromCamera}>
+                      Take Image
+                  </Button>
+          
                   </View>
+                  <Button
+                      mode="contained" // Use "outlined" for an outlined button
+                      color="#FF5733" // Set your desired button color
+                      labelStyle={styles.buttonLabel} // Apply custom label text style // Apply custom button style
+                      onPress={uploadFile}>
+                      Upload File
+                  </Button>
+                </View>
               </Card.Content>
             </Card>
           </View>
@@ -132,6 +177,16 @@ const styles2 = StyleSheet.create({
      color: '#FFF',
      fontSize: 18,
      fontWeight: 'bold'
+  },
+  activityContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute', // Apply absolute positioning
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   }
   });
 
